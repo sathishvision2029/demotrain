8f138dc07b036b1905b8911aa830a494fdf80983
sathishvision2029 <116118587+sathishvision2029@users.noreply.github.com>
2022-12-28 22:42:00 +0000

    Update samaplejob.groovy

--- jenkinsworkdsl/samaplejob.groovy
+++ jenkinsworkdsl/samaplejob.groovy
@@ -1,11 +1,2 @@
 job('example') {
-    scm {
-        github('jenkinsci/job-dsl-plugin', 'master')
-    }
-    triggers {
-        githubPush()
-    }
-    steps {
-        gradle('clean build')
-    }
 }
